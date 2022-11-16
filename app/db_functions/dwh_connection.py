8c24781cff03a3174439677e10b0fa140eaf868b
CameronHF <97699971+CameronHF@users.noreply.github.com>
2022-11-16 14:41:53 +0000

    changes to deployment

--- app/db_functions/dwh_connection.py
+++ app/db_functions/dwh_connection.py
@@ -3,6 +3,7 @@ import json
 import os
 import jaydebeapi
 from dotenv import load_dotenv
+import streamlit as st
 
 
 def get_local_configuration(database: str):
@@ -30,32 +31,6 @@ def get_local_configuration(database: str):
     return json_conf[database]
 
 
-# def dwh_connect(database: str):
-#     """
-#     creates a DWH connection object
-#     Parameters
-#     -------
-#     database: string
-#         key under which the required database connection credentials are kept in the file
-#     Returns
-#     -------
-#     dwh_conn : pyodbc connection object
-#         the connection object used to execute DWH queries
-#     """
-
-#     logging.info('Creating DWH connection')
-
-#     dwh_conf = get_local_configuration(database)
-#     print(dwh_conf)
-#     dwh_conn = jaydebeapi.connect(jclassname=dwh_conf['jclassname'],
-#                               url=dwh_conf['url'],
-#                                   driver_args={'UID': dwh_conf['user'], 'PWD': dwh_conf['password']},
-#                                  jars=dwh_conf['jar_path'])
-#     print(dwh_conn)
-    
-#     return dwh_conn
-
-
 def dwh_connect(database: str):
     """
     creates a DWH connection object
@@ -69,16 +44,11 @@ def dwh_connect(database: str):
         the connection object used to execute DWH queries
     """
 
-    logging.info('Creating DWH connection...')
+    # logging.info('Creating DWH connection')
 
     # dwh_conf = get_local_configuration(database)
-    # print(dwh_conf)
-    dwh_conn = jaydebeapi.connect(jclassname="com.cloudera.impala.jdbc.DataSource",
-                              url="jdbc:impala://coordinator-cdp-cdw-live.dw-aws-bi-live.vaxy-y10l.cloudera.site:443/default;AuthMech=3;transportMode=http;httpPath=cliservice;ssl=1;",
-                                  driver_args={'UID': "cameron.jones", 'PWD': "CnJs@1921!"},
-                                 jars="app/jars/ImpalaJDBC42.jar")
-    print(dwh_conn)
-
-
+    dwh_conn = jaydebeapi.connect(jclassname=st.secrets['DWH-CDP-JCLASSNAME'],
+                              url=st.secrets['DWH-CDP-URL'],
+                                  driver_args={'UID': st.secrets['DWH-CDP-USER'], 'PWD': st.secrets['DWH-CDP-PASSWORD']},
+                                 jars=st.secrets['DWH-CDP-JARPATH'])
     return dwh_conn
-
